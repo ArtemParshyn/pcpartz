d3101a7f27c80ccce5a2fb1a0e69667a4f452745
Artem Parshyn <parshyn4@gmail.com>
2024-09-11 09:41:38 +0300

    Product model

--- back/api/models.py
+++ back/api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.IntegerField()
+    choices = [("$", "dollar"), ("₽", "ruble"), ("€", "euro")]
+    pos_reklama = models.CharField(choices=choices, max_length=1)
+    descr = models.CharField(max_length=256)
